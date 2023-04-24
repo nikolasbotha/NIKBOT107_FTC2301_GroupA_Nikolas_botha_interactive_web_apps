48511dbc7c381b63922401af6643e078b05f9a56
Nikolas Botha <41587806+nikolasbotha@users.noreply.github.com>
2023-04-24 11:08:29 +0200

    uploading IWA 13,14,15 to repo and change IWA7 name to match others

---
+++ IWA14/script/script1.js
@@ -0,0 +1,12 @@
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+const logTwice = () => {
+  console.log(parameter)
+  console.log(parameter)
+}
+
+const parameter = (`Hello, ${firstName} (${age}). I love ${hobby}!`)
+
+logTwice ()
--- IWA_07/scripts/challenge2.js
+++
@@ -1,7 +0,0 @@
-const nickname= "Timmy";
-const firstname = "Timothy";
-
-console.log(`Good Morning, ${nickname || firstname}!`)
-
-// the problem with the code was that the console.log was using 
-// template literals using "", instead of the ` back tick notation
