d4baa52a2d5492023eaf951ead25bfc9eff03ce4
Nikolas Botha <41587806+nikolasbotha@users.noreply.github.com>
2023-04-26 16:32:56 +0200

    adding IWA15

--- IWA14/script/script1.js
+++ IWA14/script/script1.js
@@ -10,3 +10,19 @@ const logTwice = () => {
 const parameter = (`Hello, ${firstName} (${age}). I love ${hobby}!`)
 
 logTwice ()
+
+
+// firstName = 'John';
+// age = 35;
+// hobby = 'Coding';
+
+// const logTwice = () => {
+//   console(parameter)
+//   console(parameter)
+// }
+
+// function hobby () {
+//   logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+// }
+
+// hobby()
